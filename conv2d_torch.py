575e95df9a2bfc7adb1abf6c8f96fa8558c1b9f5
Xiaohu Guo <Xiaohu.Guo@amd.com>
2023-11-15 17:12:42 +0000

    add gpu

--- conv2d_torch.py
+++ conv2d_torch.py
@@ -2,10 +2,12 @@ import torch
 import torch.nn as nn
 import torch.autograd.profiler as profiler
 
-# input tensor with dimensions: batch size 1, 3 input channels, height 5, width 5
-input_tensor = torch.randn(1, 3, 224, 224)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-conv_layer = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+# input tensor with dimensions: batch size 1, 3 input channels, height 5, width 5
+input_tensor = torch.randn(1, 3, 224, 224).to(device)
+
+conv_layer = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1).to(device)
 
 # Perform the convolution operation and proifle
 with profiler.profile(record_shapes=True) as prof:
@@ -13,6 +15,7 @@ with profiler.profile(record_shapes=True) as prof:
         output_tensor = conv_layer(input_tensor)
 
 print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=10))
+print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=10))
 
 # Display the shapes of input and output tensors
 print("Input tensor shape:", input_tensor.shape)
