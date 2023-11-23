dffbb01a9ec486cee016a8d9acc05582033b4d91
Xiaohu Guo <Xiaohu.Guo@amd.com>
2023-11-23 23:41:54 +0000

    add bazel hello world examples

---
+++ bazel_examples/cpp/stage1/main/hello-world.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Output "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0; // Return 0 to indicate successful execution
+}
+
