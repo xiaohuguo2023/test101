fe2850742c8dc1b21105203aeb975712c20399d7
Xiaohu Guo <xiaohu.guo@amd.com>
2023-11-24 19:20:29 +0000

    make torch autograd profiler work

--- conv2d_torch.py
+++ conv2d_torch.py
@@ -10,12 +10,12 @@ input_tensor = torch.randn(1, 3, 224, 224).to(device)
 conv_layer = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1).to(device)
 
 # Perform the convolution operation and proifle
-with profiler.profile(record_shapes=True) as prof:
-    with profiler.record_function("model_inference"):
-        output_tensor = conv_layer(input_tensor)
+with profiler.profile(record_shapes=True, use_cuda=torch.cuda.is_available()) as prof:
+    with profiler.record_function("forward_pass"):
+         output_tensor = conv_layer(input_tensor)
+# Print profiler results
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
 
-print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=10))
-print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=10))
 
 # Display the shapes of input and output tensors
 print("Input tensor shape:", input_tensor.shape)
